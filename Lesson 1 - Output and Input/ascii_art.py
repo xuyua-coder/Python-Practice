019eab7b4011612bd6a40463ee224b27c5d5c03a
xuyua-coder <xuyua@wsc.school.nz>
2026-05-05 15:04:03 +1200

    Create ascii_art.py

---
+++ Lesson 1 - Output and Input/ascii_art.py
@@ -0,0 +1,22 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+
+
+
+
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
+print("      ______       ")
+print("    /        \     ")
+print("   |   ^   ^  |    ")
+print("    \   \_/  /     ")
+print( "       ____       " )
+print()
+
+print("^_^")
+input("Name?\n")
+print()
+print()
