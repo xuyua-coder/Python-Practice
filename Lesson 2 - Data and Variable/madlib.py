6af3776fa1004161dcabdcd09702a25482c749fb
xuyua-coder <xuyua@wsc.school.nz>
2026-05-11 12:00:02 +1200

    Create madlib.py

---
+++ Lesson 2 - Data and Variable/madlib.py
@@ -0,0 +1,23 @@
+# Create a short Madlib: Get input from your user (a bunch of words), 
+# then output a madlib using those words.
+
+# Ask user for a name and save it in a variable
+
+# Ask user for an animal and save it in a variable
+
+# Ask user for a colour and save it in a variable
+
+# Print your madlib using the 3 variables above.
+
+# ----------------------------
+
+# EXTENSION
+# Research about 'print formatting in python'. 
+# Use what you learn to rewrite your madlib into easier to read code.
+
+# ----------------------------
+
+# EXPERT (for those who already know some Python)
+# Create a randomised madlib game
+# GOAL: Just like above except...
+#       Write 4-6 different madlibs and randomise which one is output.
