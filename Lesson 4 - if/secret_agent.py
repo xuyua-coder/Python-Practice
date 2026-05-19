c53cdb89324bafaada77de174062b5a4482f8a2b
xuyua-coder <xuyua@wsc.school.nz>
2026-05-19 15:07:18 +1200

    Create secret_agent.py

---
+++ Lesson 4 - if/secret_agent.py
@@ -0,0 +1,37 @@
+### Secret Agent Login
+# Create a login process for a secret agent
+
+# Ask for the user's name and save it in a variable
+
+# Ask for the password and save it in a variable
+
+# Check if the password == 'Falcon'
+
+    # Ouput that access has been granted and welcome user using their name
+
+    # Ask for the user's age and save it in a variable
+
+    # Change the age into an integer
+
+    # If the user's age is under 13, tell them they are a spy in training
+
+    # If their age is under 18, tell them they are a junior spy
+
+    # If their age is 18 or over, tell them they are a Field Agent
+
+# Output a goodbye
+
+# ___________________________
+
+# EXTENSION
+
+# Ask more questions to give your spy more information
+# Look up how to use 'and' and 'or' to force more conditions (eg. they must be one of 3 users AND get the password correct)
+
+# ___________________________
+
+# EXPERT (For those who already know python)
+
+# Create a SPY ID GENERATOR
+# Your user must login using the correct password to access the generator
+# Use a bunch of questions to generate an id. Eg. If their name has 4 or fewer letters, their ID is a random fruit plus other logic...
