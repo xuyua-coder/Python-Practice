600669f22ec7bc3d5b9722230c97763fc378def7
xuyua-coder <xuyua@wsc.school.nz>
2026-05-25 12:00:18 +1200

    Create budget_tracker.py

---
+++ Lesson 5 - Else Elif/budget_tracker.py
@@ -0,0 +1,38 @@
+### Budget Tracker ###
+# Create a budget tracker that gives financial recommendation around an item
+
+# Create a constant to hold your budget
+# Create a constant to hold your savings (percentage) goal
+
+# Ask user for item name and save in variable
+# Ask user for cost and save in variable
+# Change the cost into an integer
+
+# Calculate the percentage of budget (cost / budget) * 100
+# Tell your user the percentage of your budget
+
+# Check if percentage is 0 and say it's free if it is
+
+# Check if the percentage is less then 10 and say it's a small treat so enjoy
+
+# Check if it is less than 50 percent and if it is tell them it's a major spend and should sleep on it
+
+# Check if it's over 100 and if it is tell them they don't have enough money
+
+# Otherwise, tell them it costs way too much and isn't worth it
+
+# _______________________
+
+# EXTENSION
+# Include an item type question and change answers based on this. 
+# Eg. food shouldn't cost as much as a bill so if it's a food, 
+# tell them to not buy it at a lower percentage
+
+
+# _______________________
+
+# EXPERT
+# Try to create a budget tracker that saves data in a file 
+# so the remaining_budget can be updated every time the program is used
+# You will need to create a save.txt file to go with this (keep it in the same folder)
+# If you're not sure how to do this check here: https://www.w3schools.com/python/python_file_write.asp 
