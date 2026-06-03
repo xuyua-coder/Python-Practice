f33e067c5c1239275c745f7da2d065ec1dfb1784
xuyua-coder <xuyua@wsc.school.nz>
2026-06-04 10:29:36 +1200

    Create uncomment_example.py

---
+++ Lesson 3 - Comment/uncomment_example.py
@@ -0,0 +1,23 @@
+# Uncomment the lines of code to test them, 
+# but be careful not to uncomment any actual comments!
+
+# Greet a person
+# name = "Skywalker"
+# greeting = "Hello there, " + name
+# print(greeting)
+
+# Calculate how many apples each person gets
+# apples = 15
+# friends = 3
+# print("Each friend gets", apples / friends, "apples.")
+
+# Calculate the numbers of months you are
+# age = input("How many years old are you? ")
+# months = int(age) * 12
+# print("You are roughly", months, "months old.")
+
+# Calculate the current price of a product
+# price = 100
+# discount = 10
+# total = price + discount 
+# print("The sale price is:", total)
