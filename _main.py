904f295fed34ad841b6b87326e188558d6069fc1
xuyua-coder <xuyua@wsc.school.nz>
2026-06-04 10:29:20 +1200

    Update _main.py

--- _main.py
+++ _main.py
@@ -2,7 +2,12 @@ print("I'm coding")
 print(5+5)
 print("5"+"5")
 print(5.0+5.0)
-print("5"+5)
-print(5+5.0)
 
+print(5+5.0)
+guess = input('What’s the password?')
+print('Checking password is a match…')
+while guess != 'secret':
+    guess = input('Try again')
+    print('Checking password is a match…')
+input('Welcome!')
 
