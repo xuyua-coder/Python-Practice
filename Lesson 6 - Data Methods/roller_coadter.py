7ee1d49c2c8c7d1be07005be99431c7e6be2dea4
xuyua-coder <xuyua@wsc.school.nz>
2026-06-04 10:29:32 +1200

    Create roller_coadter.py

---
+++ Lesson 6 - Data Methods/roller_coadter.py
@@ -0,0 +1,23 @@
+# Create a roller coaster access screener (determine if the user is allowed to ride)
+# Rules:    They must be over 150cm and over 10 years old
+#           They must not have a heart condition
+#           OR they can ride if they have a VIP pass
+
+# Get input
+
+
+
+
+
+# Check conditions and output verdict
+
+
+
+
+# ------------------------------
+# EXTENSION
+# Change your screener to work for 3 different rides (ask user which ride at the beginning) with different rules
+
+# ------------------------------
+# EXPERT
+# Follow the same task (with extension), but use dictionaries to make the code more efficient
